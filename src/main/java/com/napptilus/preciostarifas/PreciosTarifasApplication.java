d34e13c7ca8e0701aaded5218fb36ce290836854
lorenzo miret <lmiret@biobam.com>
2023-05-22 15:31:00 +0200

    project initialization

---
+++ src/main/java/com/napptilus/preciostarifas/PreciosTarifasApplication.java
@@ -0,0 +1,13 @@
+package com.napptilus.preciostarifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreciosTarifasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreciosTarifasApplication.class, args);
+	}
+
+}
