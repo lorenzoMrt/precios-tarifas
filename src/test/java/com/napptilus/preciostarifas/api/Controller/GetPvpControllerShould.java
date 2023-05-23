603d20422d07d153461bc0cd5390befd40988877
lorenzo miret <lmiret@biobam.com>
2023-05-23 17:48:36 +0200

    feat(get-product-pvp): [atdd] [outside-in] 🔴 add test case 1

---
+++ src/test/java/com/napptilus/preciostarifas/api/Controller/GetPvpControllerShould.java
@@ -0,0 +1,24 @@
+package com.napptilus.preciostarifas.api.Controller;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import com.napptilus.preciostarifas.api.ApiTestCase;
+
+public class GetPvpControllerShould extends ApiTestCase {
+
+    @Test
+    public void should_return_pvp_at_1000() throws Exception {
+        whenGetRequestSentTo("/product-pvp?date=2020-06-14-10.00.00&product-id=35455&brand-id=1");
+
+        var jsonResponse = new JSONObject()
+            .put("product-id", 35455)
+            .put("brand-id", 1)
+            .put("price-list", 1)
+            .put("start-date", "2020-06-14-00.00.00")
+            .put("end-date", "2020-12-31-23.59.59")
+            .put("price", 35.50);
+
+        assertSuccessJsonResponse(jsonResponse);
+    }
+}
